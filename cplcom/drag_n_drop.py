75989db234fd335acccfdb5759fc153dc4dd2e19
Matthew Einhorn <moiein2000@gmail.com>
2019-06-19 16:07:34 -0400

    Update drag n drop example.

--- cplcom/drag_n_drop.py
+++ cplcom/drag_n_drop.py
@@ -675,9 +675,11 @@ Factory.register('DraggableController', DraggableController)
 Factory.register('DraggableLayoutBehavior', DraggableLayoutBehavior)
 
 if __name__ == '__main__':
-    from kivy.app import runTouchApp
+    from kivy.app import App
     from kivy.uix.label import Label
     from kivy.uix.boxlayout import BoxLayout
+
+    drag_controller = DraggableController()
 
     class DraggableBoxLayout(DraggableLayoutBehavior, BoxLayout):
 
@@ -691,15 +693,27 @@ if __name__ == '__main__':
 
     class DragLabel(DraggableObjectBehavior, Label):
 
+        def __init__(self, **kwargs):
+            super(DragLabel, self).__init__(
+                **kwargs, drag_controller=drag_controller)
+
         def initiate_drag(self):
             # during a drag, we remove the widget from the original location
             self.parent.remove_widget(self)
 
-    widget = Builder.load_string('''
+    kv = '''
 BoxLayout:
     DraggableBoxLayout:
         drag_classes: ['label']
         orientation: 'vertical'
+        canvas:
+            Color:
+                rgba: (1, 1, 1, .2) if \
+app.drag_controller.dragging and app.drag_controller.widget_dragged and \
+app.drag_controller.widget_dragged.drag_cls == 'label' else (0, 0, 0, 0)
+            Rectangle:
+                pos: self.pos
+                size: self.size
         Label:
             text: 'A'
         Label:
@@ -716,6 +730,14 @@ BoxLayout:
             padding: '20dp', 0
             drag_classes: ['label2']
             orientation: 'vertical'
+            canvas:
+                Color:
+                    rgba: (1, 1, 1, .2) if \
+app.drag_controller.dragging and app.drag_controller.widget_dragged and \
+app.drag_controller.widget_dragged.drag_cls == 'label2' else (0, 0, 0, 0)
+                Rectangle:
+                    pos: self.pos
+                    size: self.size
             Label:
                 text: 'B'
             Label:
@@ -725,6 +747,12 @@ BoxLayout:
     DraggableBoxLayout:
         drag_classes: ['label', 'label2']
         orientation: 'vertical'
+        canvas:
+            Color:
+                rgba: (1, 1, 1, .2) if app.drag_controller.dragging else (0, 0, 0, 0)
+            Rectangle:
+                pos: self.pos
+                size: self.size
         DragLabel:
             text: 'A*'
             drag_cls: 'label'
@@ -746,6 +774,14 @@ BoxLayout:
         DragLabel:
             text: 'A*'
             drag_cls: 'label'
-    ''')
+    '''
 
-    runTouchApp(widget)
+
+    class MyApp(App):
+
+        drag_controller = drag_controller
+
+        def build(self):
+            return Builder.load_string(kv)
+
+    MyApp().run()
